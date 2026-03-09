407b0dd8b4c9b968dc13d47f540aaa6b5c2864df
mohakkm <mohakkmalvankar1104@gamil.com>
2026-03-09 10:30:05 +0530

    UC13: Performance Comparison

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,41 +1,36 @@
 import java.util.Scanner;
-import java.util.Stack;
 
-public class UseCase12PalindromeCheckerApp {
+public class UseCase13PalindromeCheckerApp {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input : ");
         String input = scanner.nextLine();
 
-        PalindromeStrategy strategy = new StackStrategy();
-        boolean result = strategy.isPalindrome(input);
-
-        System.out.println("Is Palindrome?: " + result);
-        scanner.close();
-    }
-}
-
-interface PalindromeStrategy {
-    boolean isPalindrome(String input);
-}
-
-class StackStrategy implements PalindromeStrategy {
-
-    public boolean isPalindrome(String input) {
         String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        Stack<Character> stack = new Stack<>();
 
-        for (int i = 0; i < normalized.length(); i++) {
-            stack.push(normalized.charAt(i));
-        }
+        long startTime = System.nanoTime();
 
-        for (int i = 0; i < normalized.length(); i++) {
-            if (normalized.charAt(i) != stack.pop()) {
-                return false;
+        boolean isPalindrome = true;
+        int start = 0;
+        int end = normalized.length() - 1;
+
+        while (start < end) {
+            if (normalized.charAt(start) != normalized.charAt(end)) {
+                isPalindrome = false;
+                break;
             }
+            start++;
+            end--;
         }
 
-        return true;
+        long endTime = System.nanoTime();
+        long executionTime = endTime - startTime;
+
+        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Execution Time : " + executionTime + " ns");
+
+        scanner.close();
     }
 }
